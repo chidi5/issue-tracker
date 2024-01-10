d7f00015a60661352bcda4e6c41facdc43075638
chidi5 <joshdash.ji@gmail.com>
2024-01-10 15:52:13 +0100

    show issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -23,6 +23,17 @@ const NewIssuepage = () => {
     const[error, setError] = useState('')
     const[isSubmitting, setSubmitting] = useState(false)
 
+    const onSubmit = handleSubmit(async (data) => {
+        try{
+            setSubmitting(true)
+            await axios.post('/api/issues', data)
+            router.push('/issues');
+        } catch(error){
+            setSubmitting(false)
+            setError('Unexpected error occurred.')
+        }
+    })
+
     return (
         <div className='max-w-xl'>
             {error && ( 
@@ -32,16 +43,7 @@ const NewIssuepage = () => {
             )}
             <form 
                 className='space-y-3'
-                onSubmit={handleSubmit(async (data) => {
-                    try{
-                        setSubmitting(true)
-                        await axios.post('/api/issues', data)
-                        router.push('/issues');
-                    } catch(error){
-                        setSubmitting(false)
-                        setError('Unexpected error occurred.')
-                    }
-                })}
+                onSubmit={onSubmit}
             >
                 <TextField.Root>
                     <TextField.Input placeholder="Title" {...register('title')} />
