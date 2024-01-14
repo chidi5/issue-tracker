b6eec98343cdec6d1801400c023d3e7be80da80b
chidi5 <joshdash.ji@gmail.com>
2024-01-14 03:19:41 +0100

    disable SSR for loading markdown editor

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,23 +1,22 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
-import { Box, Card, Flex, Heading } from '@radix-ui/themes'
-import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import ReactMarkdown from 'react-markdown'
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Box, Card, Flex } from "@radix-ui/themes";
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
   return (
-    <Box className='max-w-xl'>
-        <Skeleton />
-        <Flex gap='3' my='2'>
-            <Skeleton width='5rem' />
-            <Skeleton width='8rem' />            
-        </Flex>
-        <Card className='prose mt-4'>
-            <Skeleton count={3} />
-        </Card>
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex gap="3" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose mt-4">
+        <Skeleton count={3} />
+      </Card>
     </Box>
-  )
-}
+  );
+};
 
-export default LoadingIssueDetailPage
+export default LoadingIssueDetailPage;
