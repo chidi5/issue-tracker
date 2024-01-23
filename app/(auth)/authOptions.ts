23e608587ff3a2f3842de074c659d9b0912c246c
chidi5 <joshdash.ji@gmail.com>
2024-01-23 20:52:49 +0100

    secure the application

---
+++ app/(auth)/authOptions.ts
@@ -0,0 +1,66 @@
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
+import bcrypt from "bcrypt";
+import { NextAuthOptions } from "next-auth";
+
+const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/login",
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@smith.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // check to see if email and password is there
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        // check to see if user exists
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        });
+
+        // if no user was found
+        if (!user || !user?.hashedPassword) {
+          return null;
+        }
+
+        // check to see if password matches
+        const passwordMatch = await bcrypt.compare(
+          credentials?.password,
+          user.hashedPassword
+        );
+
+        // if password does not match
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+
+export default authOptions;
