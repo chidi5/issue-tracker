cb5261b5e2a056454b15984b11ce9b655160a41d
chidi5 <joshdash.ji@gmail.com>
2024-01-28 20:39:43 +0100

    issueStatusFilter placeholder error fix

--- app/issues/(list)/IssueStatusFilter.tsx
+++ app/issues/(list)/IssueStatusFilter.tsx
@@ -28,7 +28,7 @@ const IssueStatusFilter = () => {
         router.push("/issues" + query);
       }}
     >
-      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Trigger />
       <Select.Content>
         {statuses.map((status) => (
           <Select.Item key={status.value} value={status.value || ""}>
